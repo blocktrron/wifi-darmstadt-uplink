b429aad1678b8a352ca184fc42cb09753ce2916f
David Bauer <mail@david-bauer.net>
2020-06-24 01:38:53 +0200

    captive: limit operation instead of connection

--- wifi-darmstadt-uplink/files/lib/wifi-uplink/wifi-darmstadt/captive.sh
+++ wifi-darmstadt-uplink/files/lib/wifi-uplink/wifi-darmstadt/captive.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-curl -c "/tmp/wifidarmstadt.cookie" --connect-timeout 5 -A "Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0" -L "https://service.thecloud.eu/service-platform/macauthlogin/v1"
-curl -b "/tmp/wifidarmstadt.cookie" --connect-timeout 5 -d "terms=true" -A "Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0" -L "https://service.thecloud.eu/service-platform/macauthlogin/v1/registration"
+curl -c "/tmp/wifidarmstadt.cookie" --max-time 5 -A "Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0" -L "https://service.thecloud.eu/service-platform/macauthlogin/v1"
+curl -b "/tmp/wifidarmstadt.cookie" --max-time 5 -d "terms=true" -A "Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0" -L "https://service.thecloud.eu/service-platform/macauthlogin/v1/registration"
 rm -f /tmp/wifidarmstadt.cookie
