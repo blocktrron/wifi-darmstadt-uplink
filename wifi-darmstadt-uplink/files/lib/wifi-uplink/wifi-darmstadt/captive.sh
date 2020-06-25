2805c1f92f6513d676083571097cc8834d7dee12
David Bauer <mail@david-bauer.net>
2020-06-25 19:03:06 +0200

    captive: write response to /dev/null

--- wifi-darmstadt-uplink/files/lib/wifi-uplink/wifi-darmstadt/captive.sh
+++ wifi-darmstadt-uplink/files/lib/wifi-uplink/wifi-darmstadt/captive.sh
@@ -3,10 +3,10 @@
 COOKIEFILE="/tmp/wifidarmstadt.cookie"
 
 # Mobile
-curl -c "$COOKIEFILE" -A "Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0" -L "https://service.thecloud.eu/service-platform/redirect/13606"
+curl -s -o "/dev/null" -c "$COOKIEFILE" -A "Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0" -L "https://service.thecloud.eu/service-platform/redirect/13606"
 # Stationary
-curl -b "$COOKIEFILE" -c "$COOKIEFILE" -A "Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0" -L "https://service.thecloud.eu/service-platform/redirect/2783"
+curl -s -o "/dev/null" -b "$COOKIEFILE" -c "$COOKIEFILE" -A "Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0" -L "https://service.thecloud.eu/service-platform/redirect/2783"
 
-curl -b "$COOKIEFILE" -c "$COOKIEFILE" -A "Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0" -L "https://service.thecloud.eu/service-platform/macauthlogin/v1"
-curl -b "$COOKIEFILE" -d "terms=true" -A "Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0" -L "https://service.thecloud.eu/service-platform/macauthlogin/v1/registration"
+curl -s -o "/dev/null" -b "$COOKIEFILE" -c "$COOKIEFILE" -A "Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0" -L "https://service.thecloud.eu/service-platform/macauthlogin/v1"
+curl -s -o "/dev/null" -b "$COOKIEFILE" -d "terms=true" -A "Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0" -L "https://service.thecloud.eu/service-platform/macauthlogin/v1/registration"
 rm /tmp/wifidarmstadt.cookie
